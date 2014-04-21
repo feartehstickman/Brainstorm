0174eb032a5b297494649916555c49ea59e02e70
ablanchard17 <ablanchard1010@gmail.com>
2014-04-21 12:44:44 -0700

    Initial commit

---
+++ BrainstormProject/BrainstormProject/Engine/ComponentSystem/EngineComponentManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace BrainstormProject.Engine.ComponentSystem
+{
+    public class EngineComponentManager : IEngineComponent, ILoadable, IUpdatable, IRenderable
+    {
+        public string ComponentName { get; internal set; }
+        public int TotalComponents
+        {
+            get
+            {
+                return Components.Count;
+            }
+            internal set;
+        }
+        public int TotalActiveComponents
+        {
+            get
+            {
+                int ActiveComponents = 0;
+                for (int i = 0; i < Components.Count; ++i)
+                {
+                    
+                }
+            }
+            internal set;
+        }
+        public List<IRenderable> RenderableComponents { get; internal set; }
+        public List<IUpdatable> UpdatableComponents { get; internal set; }
+        public List<IEngineComponent> Components { get; internal set; }
+        public List<ILoadable> LoadableComponents { get; internal set; }
+
+        public EngineComponentManager()
+        {
+
+        }
+
+        public void AddComponent (IEngineComponent Component)
+        {
+            Components.Add(Component);
+
+            if (Component is ILoadable)
+            {
+                LoadableComponents.Add(Component as ILoadable);
+            }
+            if (Component is IUpdatable)
+            {
+                UpdatableComponents.Add(Component as IUpdatable);
+            }
+            if (Component is IRenderable)
+            {
+                RenderableComponents.Add(Component as IRenderable);
+            }
+        }
+    }
+}
---
+++ BrainstormProject/BrainstormProject/Engine/ComponentSystem/IEngineComponent.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+
+namespace BrainstormProject.Engine.ComponentSystem
+{
+    /// <summary>
+    /// Updatable game component
+    /// </summary>
+    public interface IUpdatable
+    {
+        void Update(GameTime gameTime);
+    }
+
+    /// <summary>
+    /// Renderable game component
+    /// </summary>
+    public interface IRenderable
+    {
+        void Render(GameTime gameTime);
+    }
+
+    /// <summary>
+    /// Engine Component
+    /// IsComponentManager() returns true if the current component is a component manager, else returns false.
+    /// EngineComponentManager is IEngineComponent, IRenderable, IUpdatable and ILoadable
+    /// </summary>
+    public interface IEngineComponent
+    {
+        bool IsComponentManager();
+        bool IsActive();
+        bool IsDead();
+    }
+
+    /// <summary>
+    /// Engine Component that needs to load things
+    /// </summary>
+    public interface ILoadable
+    {
+        void Load();
+    }
+}
