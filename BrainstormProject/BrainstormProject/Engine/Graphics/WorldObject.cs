d9315a7bbcfdfdc95cc30df1b1ed85b61db3530a
ablanchard17 <ablanchard1010@gmail.com>
2014-04-22 10:38:43 -0700

    Added: WorldObject

---
+++ BrainstormProject/BrainstormProject/Engine/Graphics/WorldObject.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Xml;
+using System.Xml.Serialization;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using BrainstormProject.Engine.ComponentSystem;
+using BrainstormProject.Engine.Graphics;
+
+namespace BrainstormProject.Engine.Graphics
+{
+    [Serializable]
+    public class WorldObject : ILoadable, IUpdatable, IRenderable, IEngineComponent
+    {
+        [XmlElement]
+        private bool Active;
+
+        [XmlElement]
+        private bool Dead;
+
+        [XmlElement]
+        public string Name { get; internal set; }
+
+        public WorldObject()
+        {
+            Active = false;
+            Dead  = false;
+        }
+
+        public virtual void Load()
+        {
+        }
+        public virtual void Update(GameTime gameTime)
+        {
+        }
+        public virtual void Render(GameTime gameTime)
+        {
+        }
+        public bool IsDead()
+        {
+            return Active;
+        }
+        public bool IsActive()
+        {
+            return Dead;
+        }
+        public bool IsComponentManager()
+        {
+            return false;
+        }
+
+        public void Activate()
+        {
+            Active = true;
+        }
+        public void Deactivate()
+        {
+            Active = false;
+        }
+    }
+}
