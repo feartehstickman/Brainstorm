a8bac0489c64a6217db20f88829638fcb52b2060
ablanchard17 <ablanchard1010@gmail.com>
2014-04-21 21:42:31 -0700

    Added: PointLight.cs

---
+++ BrainstormProject/BrainstormProject/Engine/Graphics/PointLight.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Xml.Schema;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace BrainstormProject.Engine.Graphics
+{
+    [Serializable]
+    public class PointLight
+    {
+        [XmlElement]
+        public Vector2 Position;
+
+        [XmlElement]
+        public Vector4 Color;
+
+        [XmlElement]
+        public float   Intensity;
+
+        [XmlElement]
+        public bool Active;
+
+        /// <summary>
+        /// Generic constructor
+        /// </summary>
+        public PointLight()
+        {
+        }
+
+        /// <summary>
+        /// Constructor with parameters to create point light
+        /// </summary>
+        /// <param name="Position"></param>
+        /// <param name="Color"></param>
+        /// <param name="Intensity"></param>
+        /// <param name="Active"></param>
+        public PointLight(Vector2 Position, Vector4 Color, float Intensity, bool Active)
+        {
+            this.Position  = Position;
+            this.Color     = Color;
+            this.Intensity = Intensity;
+            this.Active    = Active;
+        }
+    }
+}
