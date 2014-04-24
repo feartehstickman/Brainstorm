d27a2d58a1f1feee9c837de8bf49b0cc2923c27e
ablanchard17 <ablanchard1010@gmail.com>
2014-04-24 10:50:33 -0700

    Added: PerformanceTimer.cs

---
+++ BrainstormProject/BrainstormProject/Engine/Performance/PerformanceTimer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BrainstormProject.Engine.Performance
+{
+    public static sealed class PerformanceTimer
+    {
+        private static uint ElapsedTime;
+        private static DateTime EventBegin;
+        private static DateTime EventEnd;
+
+        public PerformanceTimer()
+        {
+            ElapsedTime = 0;
+        }
+
+        public static void BeginPerformanceEvent()
+        {
+            EventBegin = DateTime.UtcNow;
+        }
+
+        public static void EndPerformanceEvent()
+        {
+            EventEnd = DateTime.UtcNow;
+        }
+
+        public static double GetElapsedMilliseconds()
+        {
+            return (EventEnd - EventBegin).TotalMilliseconds;
+        }
+
+        public static double GetElapsedTicks()
+        {
+            return (EventEnd - EventBegin).Ticks;
+        }
+    }
+}
