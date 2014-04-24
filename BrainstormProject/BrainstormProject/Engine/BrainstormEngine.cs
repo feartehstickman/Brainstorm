796f609dcc3607862caba8acfae11f506902995a
ablanchard17 <ablanchard1010@gmail.com>
2014-04-24 11:44:02 -0700

    Add: BrainstormEngine.cs

--- BrainstormProject/BrainstormProject/Engine/BrainstormEngine.cs
+++ BrainstormProject/BrainstormProject/Engine/BrainstormEngine.cs
@@ -1,64 +1,48 @@
-﻿#region Using Statements
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
 using Microsoft.Xna.Framework.Storage;
-using Microsoft.Xna.Framework.GamerServices;
-using BrainstormProject.Engine.Graphics;
-#endregion
 
-namespace BrainstormProject
+namespace BrainstormProject.Engine
 {
-    public class Game1 : Game
+    public class BrainstormEngine : Game
     {
-        GraphicsDeviceManager graphics;
-        SpriteBatch spriteBatch;
+        private GraphicsDeviceManager GraphicsManager;
 
+        public string ApplicationName { get; internal set; }
 
-        public Game1()
-            : base()
+        public BrainstormEngine()
         {
-            graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
         }
 
         protected override void Initialize()
         {
-
-
             base.Initialize();
         }
 
-
         protected override void LoadContent()
         {
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-
-        }
-
-  
-        protected override void UnloadContent()
-        {
-            
+            base.LoadContent();
         }
 
         protected override void Update(GameTime gameTime)
         {
-
-
             base.Update(gameTime);
         }
 
-
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            
-
             base.Draw(gameTime);
         }
     }
