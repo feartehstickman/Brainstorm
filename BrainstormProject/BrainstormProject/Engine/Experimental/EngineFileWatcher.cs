cfb8d476e26aeeb0c889c15ec82b58f11a8a087e
ablanchard17 <ablanchard1010@gmail.com>
2014-04-27 09:59:55 -0700

    Added: EngineFileWatcher.cs Deleted: LoadFromFile.cs ( shitty code that won't do shit even though its shitty.

---
+++ BrainstormProject/BrainstormProject/Engine/Experimental/EngineFileWatcher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace BrainstormProject.Engine.Experimental
+{
+    /// <summary>
+    /// Data required to determine if a file has been written to since we have checked last
+    /// </summary>
+    public struct FileWatchData
+    {
+        public string   FilePath;
+        public DateTime LastAccess;
+        public DateTime CachedLastAccess;
+
+        /// <summary>
+        /// Initializes FileWatchData Structure
+        /// </summary>
+        /// <param name="FilePath">Path of file to watch. Throws FileNotFoundException</param>
+        public FileWatchData(string FilePath)
+        {
+            if(!File.Exists(FilePath))
+                throw new FileNotFoundException();
+
+            this.FilePath = FilePath;
+
+            LastAccess       = File.GetLastWriteTimeUtc(FilePath);
+            CachedLastAccess = File.GetLastWriteTimeUtc(FilePath);
+        }
+    }
+
+    public class EngineFileWatcher
+    {
+        private Timer FileWatchTask;
+    }
+}
